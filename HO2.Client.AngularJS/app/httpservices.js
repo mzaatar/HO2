4b5e5b94c7aea5032f7a0c4c20c9ff79c6a16d9c
Mohamed Zaatar <mohamed.zaatar@gmail.com>
2015-10-15 12:54:39 +0800

    Data Service Finalized

--- HO2.Client.AngularJS/app/httpservices.js
+++ HO2.Client.AngularJS/app/httpservices.js
@@ -1,49 +1,76 @@
 ﻿// dataservice factory
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('dataservice', dataservice, ['$http']);
+
+    /* @ngInject */
+
+    function dataservice($http) {
+
+        var baseUrl = 'http://localhost:49588/api/mates';
+
+        function functionFailed(error) {
+            console.log('XHR Failed. Error is :' + error);
+        }
 
 
-angular
-    .module('app')
-    .factory('dataservice', dataservice, ['$http']);
-
-//dataservice.$inject = ['$http'];
-
-function dataservice($http) {
-
-    var baseUrl = 'http://localhost:49588/api/mates';
-
- 
-    function getMateById(id) {
-        return $http.get(baseUrl + "/" + id);
-    };
-
-    function getAllMates() {
-        return $http.get(baseUrl)
-            .then(function(response) {
+        function getMateById(id) {
+            function getMateByIdComplete(response) {
+                console.log("Mate searching for " + id);
                 return response.data;
-            })
-            .catch(function(error) {
-                return error;
-            });
-    }
+            }
 
-    function addMateWithDetails(newmate) {
-        $http.post(baseUrl + "/", newmate)
-            .then(function(response) {
-                return response;
-            });
-    }
+            return $http.get(baseUrl + "/" + id)
+                .then(getMateByIdComplete)
+                .catch(functionFailed);
+        };
 
-    function updateMateWithDetails(mate) {
-        $http.put(baseUrl + "/", mate)
-            .then(function(response) {
-                return response;
-            });
-    }
+        function getAllMates() {
+            function getAllMatesComplete(response) {
+                console.log("get All mates");
+                return response.data;
+            }
+            return $http.get(baseUrl)
+                .then(getAllMatesComplete)
+                .catch(functionFailed);
+        }
 
-    return {
-        getMateById: getMateById,
-        getAllMates: getAllMates,
-        addMateWithDetails: addMateWithDetails,
-        updateMateWithDetails: updateMateWithDetails
-    };
-}
+
+        function addMateWithDetails(newmate) {
+            function addMateWithDetailsComplete(response) {
+                console.log("Addedd successfully");
+                return response.data;
+            }
+
+            console.log("inserting a new user with these details : {" + newmate.email + "} , {" + newmate.firstname + "} , {" + newmate.lastname + "}");
+
+            return $http.post(baseUrl + "/", newmate)
+                .then(addMateWithDetailsComplete)
+                .catch(functionFailed);
+        }
+
+
+        function updateMateWithDetails(mate) {
+            function updateMateWithDetailsComplete(response) {
+                console.log("Udpated successfully");
+                return response.data;
+            }
+
+            return $http.put(baseUrl + "/", mate)
+                .then(updateMateWithDetailsComplete)
+                .catch(functionFailed);
+        }
+
+        var service = {
+            getMateById: getMateById,
+            getAllMates: getAllMates,
+            addMateWithDetails: addMateWithDetails,
+            updateMateWithDetails: updateMateWithDetails
+        };
+
+        return service;
+    }
+})();
--- HO2.Client.AngularJS/app/mates/mates.controller.js
+++ HO2.Client.AngularJS/app/mates/mates.controller.js
@@ -11,10 +11,6 @@
         console.log("I'm in MatesController");
         var vm = this;
 
-        var onGetAllComplete = function (response) {
-            vm.mateList = response.data;
-        };
-
         var onAdded = function (response) {
             console.log("Addedd successfully");
         };
@@ -30,35 +26,27 @@
 
 
         vm.search = function (id) {
-            console.log("searching for " + id);
-
-            dataservice.getMateById(id)
-             .success(function (data) {
-                vm.mate = data;
-            })
-            .error(function (error) {
-                console.log('Unable to load customer data: ' + error.message);
-          });
+            return dataservice.getMateById(id)
+                .then(function(data) {
+                    vm.mate = data;
+                    return vm.mate;
+                });
         };
-
 
         vm.getAllMates = function () {
-            console.log("get All mates ");
-            $http.get("http://localhost:49588/api/mates")
-                .then(onGetAllComplete, onError);
+            return dataservice.getAllMates()
+                .then(function (data) {
+                    vm.mateList = data;
+                    return vm.mateList;
+                });
         };
 
-
-        vm.addMate = function (newmate) {
-            console.log("inserting a new user with these details : {" + newmate.email + "} , {" + newmate.firstname + "} , {" + newmate.lastname + "}");
-            $http.post("http://localhost:49588/api/mates/", newmate)
-                .then(onAdded, onError);
+        vm.addMate = function(newmate) {
+            return dataservice.addMateWithDetails(newmate);
         };
 
         vm.updateMate = function (mate) {
-            console.log("inserting a new user with these details : {" + mate.Id + "} , {" + mate.Email + "} , {" + mate.Firstname + "} , {" + mate.Lastname + "}");
-            $http.put("http://localhost:49588/api/mates/", mate)
-                .then(onUpdated, onError);
+            return dataservice.updateMateWithDetails(mate);
         };
 
     };
